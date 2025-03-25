e9ce95b73215d61e2e88d4647bbd3ba114795ef6
Masuma-N <113317957+Masuma-N@users.noreply.github.com>
2025-03-25 00:19:28 -0400

    styled tab bar to the theme

--- Fitpalzsz/FitpalzszApp.swift
+++ Fitpalzsz/FitpalzszApp.swift
@@ -5,10 +5,23 @@
 //  Created by Naomi Talukder on 3/24/25.
 //
 
-import SwiftUI
+import SwiftUI 
 
 @main
 struct FitpalzszApp: App {
+    init() {
+           let appearance = UITabBarAppearance()
+           appearance.configureWithOpaqueBackground()
+           appearance.backgroundColor = UIColor.black
+           UITabBar.appearance().standardAppearance = appearance
+           
+           if #available(iOS 15.0, *) { // ios 14 or earlir will crash
+               UITabBar.appearance().scrollEdgeAppearance = appearance
+           }
+
+           UITabBar.appearance().tintColor = UIColor.white
+           UITabBar.appearance().unselectedItemTintColor = UIColor.gray
+       }
     var body: some Scene {
         WindowGroup {
             ContentView()
